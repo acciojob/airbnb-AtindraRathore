34e7c2ba75049deac8b43eaf499bc94d9b9e79dd
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-07-04 23:42:58 +0530

    b

--- src/main/java/com/driver/controllers/HotelManagementRepository.java
+++ src/main/java/com/driver/controllers/HotelManagementRepository.java
@@ -4,117 +4,84 @@ import com.driver.model.Booking;
 import com.driver.model.Facility;
 import com.driver.model.Hotel;
 import com.driver.model.User;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class HotelManagementRepository {
-    Map<String, Hotel> hoteldb=new HashMap<>();
-    Map<Integer, User> Userdb=new HashMap<>();
-    Map<String, Booking> bookingroomdb=new HashMap<>();
-
-    public String addHotel(Hotel hotel){
-
-        //You need to add an hotel to the database
-        //incase the hotelName is null or the hotel Object is null return an empty a FAILURE
-        //Incase somebody is trying to add the duplicate hotelName return FAILURE
-        //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
-        if(hotel.getHotelName() == null){
-            return "FAILURE";
-        }
-        if(hoteldb.containsKey(hotel.getHotelName())){
-            return "FAILURE";
-        }
-        hoteldb.put(hotel.getHotelName(),hotel);
+    Map<String, Hotel> hotelMap = new HashMap<>(); //hotelName -> hotel
+    Map<Integer, User> userMap = new HashMap<>(); // adharNo -> user
+    Map<String, Booking> bookingMap = new HashMap<>(); //BookingId -> booking
+    Map<String, Integer> userRent = new HashMap<>(); // BookingId -> amountToBePaid
+    public String addHotel(Hotel hotel) {
+        if (hotel.getHotelName() == null)return "FAILURE";
+        if (hotelMap.containsKey(hotel.getHotelName()))return "FAILURE";
+        String hotelName = hotel.getHotelName();
+        hotelMap.put(hotelName, hotel);
         return "SUCCESS";
     }
 
-
-    public Integer addUser(User user){
-
-        //You need to add a User Object to the database
-        //Assume that user will always be a valid user and return the aadharCardNo of the user
-        Userdb.put(user.getaadharCardNo(),user);
-        return user.getaadharCardNo();
+    public Integer addUser(User user) {
+        int adharNum = user.getaadharCardNo();
+        userMap.put(adharNum, user);
+        return adharNum;
     }
 
-
-    public String getHotelWithMostFacilities(){
-
-        //Out of all the hotels we have added so far, we need to find the hotelName with most no of facilities
-        //Incase there is a tie return the lexicographically smaller hotelName
-        //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
-        int max = 0;
-         List<String> hotelname=new ArrayList<>();
-        for(String  hotelName : hoteldb.keySet()){
-            int facilities=(hoteldb.get(hotelName).getFacilities().size());
-            if(facilities>max){
-                max=facilities;
-            }
+    public String getHotelWithMostFacilities() {
+        int maxFacility = 0;
+        for (String key : hotelMap.keySet()) {
+            List<Facility> facilities = hotelMap.get(key).getFacilities();
+            maxFacility = Math.max(maxFacility, facilities.size());
         }
-        if(max==0) return "";
-        for(String HotelName: hoteldb.keySet()){
-            if(max == hoteldb.get(HotelName).getFacilities().size()){
-                hotelname.add(HotelName);
-            }
+
+        if (maxFacility == 0) return "";
+        List<String> hotelNames = new ArrayList<>();
+        for (String key : hotelMap.keySet()) {
+            List<Facility> facilities = hotelMap.get(key).getFacilities();
+            if (facilities.size() == maxFacility) hotelNames.add(key);
         }
-        Collections.sort(hotelname);
-        return hotelname.get(0);
+        Collections.sort(hotelNames);
+        return hotelNames.get(0);
     }
 
-
-    public int bookARoom( Booking booking){
-
-        //The booking object coming from postman will have all the attributes except bookingId and amountToBePaid;
-        //Have bookingId as a random UUID generated String
-        //save the booking Entity and keep the bookingId as a primary key
-        //Calculate the total amount paid by the person based on no. of rooms booked and price of the room per night.
-        //If there are not enough rooms available in the hotel that we are trying to book return -1
-        //in other case return total amount paid
-        UUID uuid=UUID.randomUUID();
-        String bookingId=String.valueOf(uuid);
-        booking.setBookingId(bookingId);
-        bookingroomdb.put(bookingId,booking);
-        int priceperroom = hoteldb.get(booking.getHotelName()).getPricePerNight();
-        int no_of_rooms = booking.getNoOfRooms();
-        if(no_of_rooms>hoteldb.get(booking.getHotelName()).getAvailableRooms()){
-            return -1;
+    public int bookRoom(Booking booking) {
+        String hotelName = booking.getHotelName();
+        if (!hotelMap.containsKey(hotelName))return -1;
+        if (hotelMap.get(hotelName).getAvailableRooms() >= booking.getNoOfRooms()) {
+            Hotel hotel = hotelMap.get(hotelName);
+            int totalRoomAvilable = hotel.getAvailableRooms();
+            totalRoomAvilable -= booking.getNoOfRooms();
+            hotel.setAvailableRooms(totalRoomAvilable);
+            hotelMap.put(hotelName, hotel);
+            String bookingId = UUID.randomUUID() + "";
+            System.out.println(bookingId + "bookingId");
+            int amountTobePaid = hotel.getPricePerNight() * booking.getNoOfRooms();
+            bookingMap.put(bookingId, booking);
+            userRent.put(bookingId, amountTobePaid);
+            System.out.println(amountTobePaid + "Amount To Paid");
+            return amountTobePaid;
         }
-        booking.setAmountToBePaid(priceperroom*no_of_rooms);
-        return booking.getAmountToBePaid();
+        return -1;
     }
 
-
-    public int getBookings(Integer aadharCard)
-    {
-        //In this function return the bookings done by a person
-        //count return
-        int count = 0;
-        for(String bookingId : bookingroomdb.keySet()){
-            count++;
+    public int getBookings(Integer aadharCard) {
+        int cnt = 0;
+        for (String key : bookingMap.keySet()) {
+            if (aadharCard.equals(bookingMap.get(key).getBookingAadharCard()))cnt++;
         }
-        return count;
+        return cnt;
     }
 
-
-    public Hotel updateFacilities(List<Facility> newFacilities,String hotelName){
-
-        //We are having a new facilites that a hotel is planning to bring.
-        //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
-        //return the final updated List of facilities and also update that in your hotelDb
-        //Note that newFacilities can also have duplicate facilities possible
-        Hotel hotel = hoteldb.get(hotelName);
+    public Hotel updateFacilities(List<Facility> newFacilities, String hotelName) {
+        if (!hotelMap.containsKey(hotelName))return null;
+        Hotel hotel = hotelMap.get(hotelName);
         List<Facility> facilities = hotel.getFacilities();
-        HashSet setlist=new HashSet(facilities);
-        for(Facility facility: newFacilities){
-            if(facilities.contains(facility)==false){
-                facilities.add(facility);
-            }
+        for (int i = 0; i < newFacilities.size(); i++) {
+            if (!facilities.contains(newFacilities.get(i)))facilities.add(newFacilities.get(i));
         }
-        List<Facility> facilityList=new ArrayList<>(setlist);
-        hotel.setFacilities(facilityList);
+        hotel.setFacilities(facilities);
+        hotelMap.put(hotelName, hotel);
         return hotel;
     }
-
 }
-
--- src/main/java/com/driver/controllers/HotelManagementService.java
+++ src/main/java/com/driver/controllers/HotelManagementService.java
@@ -4,65 +4,36 @@ import com.driver.model.Booking;
 import com.driver.model.Facility;
 import com.driver.model.Hotel;
 import com.driver.model.User;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.List;
 
+@Service
 public class HotelManagementService {
 
-      HotelManagementRepository hotelManagementRepository=new HotelManagementRepository();
-
-        public String addHotel( Hotel hotel){
-
-            //You need to add an hotel to the database
-            //incase the hotelName is null or the hotel Object is null return an empty a FAILURE
-            //Incase somebody is trying to add the duplicate hotelName return FAILURE
-            //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
-           return hotelManagementRepository.addHotel(hotel);
-        }
-        public Integer addUser( User user){
-
-            //You need to add a User Object to the database
-            //Assume that user will always be a valid user and return the aadharCardNo of the user
-             return hotelManagementRepository.addUser(user);
-        }
-        public String getHotelWithMostFacilities(){
-
-            //Out of all the hotels we have added so far, we need to find the hotelName with most no of facilities
-            //Incase there is a tie return the lexicographically smaller hotelName
-            //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
-
-
-            return hotelManagementRepository.getHotelWithMostFacilities();
-        }
-        public int bookARoom( Booking booking){
-
-            //The booking object coming from postman will have all the attributes except bookingId and amountToBePaid;
-            //Have bookingId as a random UUID generated String
-            //save the booking Entity and keep the bookingId as a primary key
-            //Calculate the total amount paid by the person based on no. of rooms booked and price of the room per night.
-            //If there are not enough rooms available in the hotel that we are trying to book return -1
-            //in other case return total amount paid
-             return hotelManagementRepository.bookARoom(booking);
-        }
-        public int getBookings(Integer aadharCard)
-        {
-            //In this function return the bookings done by a person
-            //count return
-          return hotelManagementRepository.getBookings(aadharCard);
-        }
-        public Hotel updateFacilities(List<Facility> newFacilities, String hotelName){
-
-            //We are having a new facilites that a hotel is planning to bring.
-            //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
-            //return the final updated List of facilities and also update that in your hotelDb
-            //Note that newFacilities can also have duplicate facilities possible
-
-            return hotelManagementRepository.updateFacilities(newFacilities,hotelName);
-        }
-
-
+    HotelManagementRepository hotelManagementRepoaitory = new HotelManagementRepository();
+    public String addHotel(Hotel hotel) {
+        return hotelManagementRepoaitory.addHotel(hotel);
     }
 
+    public Integer addUser(User user) {
+        return hotelManagementRepoaitory.addUser(user);
+    }
 
+    public String getHotelWithMostFacilities() {
+        return hotelManagementRepoaitory.getHotelWithMostFacilities();
+    }
+
+    public int bookRoom(Booking booking) {
+        return hotelManagementRepoaitory.bookRoom(booking);
+    }
+
+    public int getBookings(Integer aadharCard) {
+        return hotelManagementRepoaitory.getBookings(aadharCard);
+    }
+
+    public Hotel updateFacilities(List<Facility> newFacilities, String hotelName) {
+        return hotelManagementRepoaitory.updateFacilities(newFacilities, hotelName);
+    }
+}
